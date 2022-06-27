73c112fb99e064267551c82943e099111b7b9b02
ad.medronha@gmail.com <ad.medronha@gmail.com>
2022-06-27 17:34:09 -0300

    main.py

--- src/main.py
+++ src/main.py
@@ -7,20 +7,22 @@ from PIL import Image
 import style
 
 st.title('Transferência de Estilo')
-prev_path = os.path.abspath(os.path.join(".."))
 
+prev_path = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
 imgs_to_infer_path = os.path.join(prev_path, "images", "in")
 img_file_names = os.listdir(imgs_to_infer_path)
 img = st.sidebar.selectbox(
     'Selecione a imagem', img_file_names
 )
-
 model_path = os.path.join(prev_path, "models", "coco2017")
 model_file_names = os.listdir(model_path)
 model_file_names = [file[:-6] for file in model_file_names]
 style_name = st.sidebar.selectbox(
     'Selecione o tipo', model_file_names
 )
+
+
+
 
 model_path = os.path.join(prev_path, model_path, style_name + ".model")
 model = model_path
